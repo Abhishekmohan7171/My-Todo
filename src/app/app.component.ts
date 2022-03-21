b8e1f1572f7a701426280c3be76ccc15d848edaf
Abhishekmohan7171 <abhishekmohan7171@gmail.com>
2022-03-21 12:33:16 +0530

    main commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Todo } from './models/Todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-todo';
+
+  inputTodo:string | undefined;
+
+  todos:any;
+
+  ngOnInit():void {
+    this.todos = [
+      {
+        content:"First Task",
+        completed: true
+      },
+      {
+        content:"Second Task",
+        completed: false
+      }
+    ]
+  }
+//  add todo
+  addTodo(){
+    this.todos.push({
+      content:this.inputTodo,
+      completed:false
+    })
+    this.inputTodo = '';
+    // console.log(this.inputTodo)
+  }
+  
+// delte todo
+  deleteTodo (id: number){
+    this.todos = this.todos.filter((v:any,i:any) => i !== id);
+  }
+
+
 }
